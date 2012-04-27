2c70742971e23c2535d7e337bc5f8e7ea857da5d
Tibor Udvari <tibor.udvari@he-arc.ch>
2012-04-27 20:50:01 +0200

    Small refactoring

--- stats.py
+++ stats.py
@@ -2,32 +2,37 @@ import sys
 
 """ Takes a file as argument """
 
-fileName = "testfile.txt"
-
-try:
-    f = open(fileName, "r")
-except IOError as e:
-    sys.exit("IO Error has occured. Try again ... ")
-
-words = {}
-
-print(type(words))
-
-while True:
-    c = f.read(1)
-    if not c :  break;
-    if c in words :
-        words[c] += 1
-    else:
-        words[c] = 1
-
-print("Finished generating statistics")
+def getStatistics(File):
+    """ Takes a file in argument and returns statistics in the form of a dictionary"""
+    statistics = {}
+    while True:
+        c = f.read(1)
+        if not c :  break;
+        if c in statistics :
+            statistics[c] += 1
+        else:
+            statistics[c] = 1
     
-f.close()
+    f.close()
+    print("Finished generating statistics.")
+    return statistics
 
-print("Printing statistics : ")
+def printStats(statistics):
+    #TODO see how to print \n for exaample
+    print("Printing statistics : ")
+    for character in statistics.keys():
+        print(" \" ",character," \" ", " |  ",  statistics[character], end="\n")
 
-for character in words.keys():
-    print(" \" ",character," \" ", " |  ",  words[character], end="\n")
-    
-    
+if __name__ == "__main__":
+
+    fileName = "testfile.txt"
+
+    try:
+        f = open(fileName, "r")
+    except IOError as e:
+        sys.exit("IO Error has occured. Try again ... ")
+    except Exception as e:
+        sys.exit("Exception has occured " + e)
+
+    statistics = getStatistics(f)
+    printStats(statistics)
